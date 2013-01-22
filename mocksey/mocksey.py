e885cd2f88b35563558b881417a8f47dcaf6308d
Chris McGraw <chris.mcgraw@lodgenet.com>
2013-01-21 22:47:20 -0600

    Adds basic property mocking
    Adds reflection of properties including no-arg methods
    
    I even wrote half of this with vim on an iPad on-screen keyboard over ssh.
    Sombody owes me a beer ;)

--- mocksey/mocksey.py
+++ mocksey/mocksey.py
@@ -1,9 +1,20 @@
 class MockseyObject(object):
-    pass
+
+    def __repr__(self):
+        return "Mock%s" % self.__class__.__name__
+
+    def stub_method(self):
+        pass
 
 
 def generate_mock(subject):
 
     mock = MockseyObject()
     mock.__class__.__name__ = subject.__name__
+    for attr in dir(subject):
+        if len(attr) > 1 and attr[1] != '_':
+            val = getattr(subject, attr)
+            if callable(val):
+                val = mock.stub_method
+            setattr(mock, attr, val)
     return mock
--- mocksey/tests/test_simple_mock.py
+++ mocksey/tests/test_simple_mock.py
@@ -1,23 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from mocksey import generate_mock
+from mocksey import generate_mock  # SUT
 
+import random
 import unittest
 
+TEETH_PROP = '%d wombat teeth are full of ouch' % random.randint(0, 30)
 
-class BoisterousBagel(object):
-    pass
+
+class WileyWombat(object):
+    teeth = TEETH_PROP
+
+    def masticate(self):
+        while True:
+            pass
 
 
 class SimpleMockTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.mock = generate_mock(BoisterousBagel)
+        self.mock = generate_mock(WileyWombat)
 
     def test_mock_steals_class_name(self):
         """ mocksey.generate_mock: Mock "inherits" mocked class name"""
-        self.assertEqual('BoisterousBagel', self.mock.__class__.__name__)
+        self.assertEqual('WileyWombat', self.mock.__class__.__name__, "Mock did not inherit class name")
+
+    def test_mock_reports_itself_as_mock(self):
+        """ mocksey.generate_mock: Mock's repr reveals it's true nature"""
+        self.assertEqual("MockWileyWombat", '%s' % self.mock, "Mock did not identify as mock in repr")
+
+    def test_mock_inherits_attributes(self):
+        """ mocksey.generate_mock: Attributes on mocked class get mirrored onto mock"""
+        self.assertTrue(hasattr(self.mock, 'teeth'), "Mock does not have an attribute that the mocked object did")
+        self.assertEqual(TEETH_PROP, self.mock.teeth, "Mock's value was not set correctly.")
+
+    def test_mock_inherits_callable(self):
+        """ mocksey.generate_mock: Methods on mocked class are mirrored and emptied"""
+        self.assertTrue(hasattr(self.mock, 'masticate'), "Mock does not have a method that the mocked object did")
+        self.mock.masticate()
 
 if __name__ == '__main__':
     unittest.main()
